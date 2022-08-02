ff26553b226ad811e04c5f0cb05fee7c7c307dd2
schmwong <smwong87@gmail.com>
2022-08-02 16:36:57 +0800

    moved csv.reader out of nested if block

--- log_concat.py
+++ log_concat.py
@@ -33,16 +33,16 @@ def update_logs(instance):
     filepath = f"./login-log/{filename.split()[1].split('_')[0]}/{filename}"  # ./<folder name>/<filename>
 
     if os.path.exists(filename):
+        # ----------------------------------------------------------------
+        # For debugging new file
+        print(f"\n\nReading update file: {filename}...\n\n")
+        with open(filename, "r", newline="", encoding="utf-8") as file:
+            reader = csv.reader(file)
+            for row in reader:
+                print(row)
+        # ----------------------------------------------------------------
         if os.path.exists(filepath):
             df_old = pd.read_csv(filepath, index_col=False, encoding="utf-8")
-            # ----------------------------------------------------------------
-            # For debugging new file
-            print(f"\n\nReading update file: {filename}...\n\n")
-            with open(filename, "r", newline="", encoding="utf-8") as file:
-                reader = csv.reader(file)
-                for row in reader:
-                    print(row)
-            # ----------------------------------------------------------------
             df_update = pd.read_csv(filename, index_col=False, encoding="utf-8")
             df_new = pd.concat([df_old, df_update])
             df_new.drop_duplicates(inplace=True)
