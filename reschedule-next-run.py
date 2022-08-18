043df50b06582a136f3d08cb800443ff3972b8d3
schmwong <smwong87@gmail.com>
2022-08-18 14:38:24 +0800

    added `print` statements to show changes in cron

--- reschedule-next-run.py
+++ reschedule-next-run.py
@@ -10,6 +10,9 @@ from ruamel.yaml import YAML
 # import env variable from runner's bash shell
 folder = os.environ["folder"]
 
+current_cron = []
+new_cron = []
+
 workflow_file = f"./.github/workflows/login-{folder}-auto.yml"
 yaml = YAML()  # defaults to round-trip (typ="rt")
 
@@ -19,6 +22,7 @@ with open(workflow_file, "r") as file:
 
     # edit cron strings
     for schedule in schedules:
+        current_cron.append(schedule["cron"])
         cron = (schedule["cron"]).split()
         new_hour = int(cron[1]) + 1
         if new_hour > 23:
@@ -30,7 +34,11 @@ with open(workflow_file, "r") as file:
         cron[1] = new_hour
         cron = " ".join(cron)
         schedule["cron"] = cron
+        new_cron.append(cron)
 
+print(f"Current cron times are:\n{current_cron}\n\n")
+print("\nAdding one hour...\n")
+print(f"New cron times are:\n{new_cron}\n\n")
 
 with open(workflow_file, "w") as file:
     yaml.dump(wf, file)
